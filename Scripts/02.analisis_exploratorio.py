d19bda6ebf0a1c3fd7d01d2e5827c89e873d2ec6
dfsaker <dfsaker7@gmail.com>
2025-11-27 17:42:43 -0300

    Conclusiones

--- Scripts/02.analisis_exploratorio.py
+++ Scripts/02.analisis_exploratorio.py
@@ -48,3 +48,23 @@ Productividad_General=dataframe["ProductivityIndex"].mean()
 print(f"EL ÍNDICE DE PRODUCTIVIDAD PROMEDIO GENERAL DE LA EMPRESA ES: {Productividad_General:.3f}")
 
 
+# ---------------------------------EMPLEADO CON MAYOR PRODUCTIVIDAD---------------------------------------------
+
+# 3. IDENTIFICAR LA FILA CON EL VALOR MÁXIMO
+
+# Usamos idxmax() para encontrar el índice de la fila con el valor más alto en la columna 'ProductivityIndex'
+
+indice_max_prod = dataframe['ProductivityIndex'].idxmax()
+
+# 4. EXTRAER LA INFORMACIÓN DEL EMPLEADO MÁS PRODUCTIVO
+
+empleado_mas_productivo = dataframe.loc[indice_max_prod]
+
+# 5. MOSTRAR EL RESULTADO
+
+print("--- EMPLEADO CON LA MAYOR PRODUCTIVIDAD ---")
+
+print(empleado_mas_productivo[['EmployeeID', 'Department', 'HoursPerWeek', 'PerformanceScore', 'Satisfaction', 'ProductivityIndex']])
+
+
+print(dataframe['ProductivityIndex'].min())
